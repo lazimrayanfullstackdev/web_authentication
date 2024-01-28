217f5462de620fd312b8f16ea6903f06db925802
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2024-01-28 13:09:11 +0600

    Database Added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "Lazimrayan99*",
+  port: 5433,  
+})
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,17 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const  email = req.body.username;
+  const password = req.body.password;
+  console.log("Username is: "+email+" and Password is: "+password);
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  console.log("Username is: "+email+" and Password is: "+password);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
